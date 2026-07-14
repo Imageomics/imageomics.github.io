defbe0e6fc1c3388cf196aa8976b208402cde6f6
Jason Amo-Mensah <jasonam84@gmail.com>
2026-07-13 22:20:08 -0400

    Fix WIP banner coverage and favicon

--- js/load-components.js
+++ js/load-components.js
@@ -2,7 +2,7 @@
 (function() {
     'use strict';
 
-    const COMPONENT_CACHE_VERSION = '4';
+    const COMPONENT_CACHE_VERSION = '5';
 
     function getPathPrefix() {
         const path = window.location.pathname;
@@ -66,6 +66,12 @@
         target.innerHTML = adjustPaths(html, prefix);
 
         if (targetId === 'header-placeholder') {
+            const notice = document.createElement('div');
+            notice.className = 'site-notice';
+            notice.setAttribute('role', 'status');
+            notice.innerHTML = '<strong>Work in progress</strong><span aria-hidden="true">&mdash;</span><span>this website is still being built.</span>';
+            target.prepend(notice);
+
             if (typeof window.initHamburger === 'function') {
                 window.initHamburger();
             }
@@ -111,7 +117,7 @@
         const favicon = document.createElement('link');
         favicon.rel = 'icon';
         favicon.type = 'image/png';
-        favicon.href = `${prefix}images/favicon.png`;
+        favicon.href = `${prefix}images/Imageomics-logo.png`;
         document.head.appendChild(favicon);
     }
 
